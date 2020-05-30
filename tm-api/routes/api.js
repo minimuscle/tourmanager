e67b8af6fd036731e92fe828fc6429be057de173
Justin <justinttphan@gmail.com>
2020-05-30 16:19:45 +1000

    Added post functions. Fixed syntax.

--- tm-api/routes/api.js
+++ tm-api/routes/api.js
@@ -6,35 +6,57 @@ const locations = require("../jsons/locations.json");
 const tours = require("../jsons/tours.json");
 const users = require("../jsons/users.json");
 
-router.get('/', function(req, res, next){
+function checkDuplicate(data, input) {
+    var matches = 0
+    Object.keys(data).forEach(function(key) {
+      if(input == data[key].name){
+        matches++ 
+      }
+    })
+    return matches
+}
+
+function validateJson(input) {
+
+}
+
+
+router.get('/', function(req, res, next) {
 	console.log("GET recieved")
-	res.send("GET recieved");
+	res.send("GET recieved")
 });
 
-router.get('/locations', function(req, res, next){
-
+router.get('/locations', function(req, res, next) {
     res.json({
         locations: locations
     })
 });
 
-router.get('/tours', function(req, res, next){
-
+router.get('/tours', function(req, res, next) {
     res.json({
         tours: tours
     })
 });
 
-router.get('/users', function(req, res, next){
-
+router.get('/users', function(req, res, next) {
     res.json({
         users: users
     })
 });
 
-router.post('/user', function(req, res, next){
-	console.log(req.body)
-	res.send("POST recieved")
+
+router.post('/locations', function(req, res, next) {
+    isDuplicate = checkDuplicate(locations, req.body.name)
+    isValidJson = validateJson(req.body)
+    
+    if(!isDuplicate) {
+        locations.push(req.body)
+        res.json({
+            locations: locations
+        })
+    } else {
+        res.send("ERROR: Duplicate Entry")
+    }
 });
 
 module.exports = router;
