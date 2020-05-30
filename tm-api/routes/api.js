47b6ec534fbefafaed864031e042ea6f6dc1492d
Justin <justinttphan@gmail.com>
2020-05-30 14:48:09 +1000

    Minor fixes.

--- tm-api/routes/api.js
+++ tm-api/routes/api.js
@@ -2,33 +2,39 @@ const express = require('express');
 const request = require('request');
 const router = express.Router();
 
+const locations = require("../jsons/locations.json");
+const tours = require("../jsons/tours.json");
+const users = require("../jsons/users.json");
+
 router.get('/', function(req, res, next){
 	console.log("GET recieved")
 	res.send("GET recieved");
 });
 
 router.get('/locations', function(req, res, next){
-    location = require("../jsons/locations.json");
 
     res.json({
-        data: location
+        locations: locations
     })
 });
 
 router.get('/tours', function(req, res, next){
-    tour = require("../jsons/tours.json");
 
     res.json({
-        data: tour
+        tours: tours
     })
 });
 
 router.get('/users', function(req, res, next){
-	user = require("../jsons/users.json");
 
     res.json({
-        data: user
+        users: users
     })
+});
+
+router.post('/user', function(req, res, next){
+	console.log(req.body)
+	res.send("POST recieved")
 });
 
 module.exports = router;
