fcf17455f4dbd09375b9fb836ac16d887c0bcb26
Liam Carnie <37321239+LiamCarnie@users.noreply.github.com>
2020-05-31 13:06:47 +1000

    Added sprint suites

---
+++ tm-backend/Tests/Sprint3Suite.java
@@ -0,0 +1,10 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({TourTests.class, LoginTests.class})
+
+public class Sprint3Suite {
+
+}
